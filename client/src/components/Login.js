368f02934f0f925d73c9213f8c6b1d989d2f0e53
rjcrter11 <rjcrter11@gmail.com>
2020-02-28 10:23:55 -0600

    login form successfully recieves token

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,45 @@
 import React from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { withFormik, Form, Field } from "formik";
+import { withRouter } from "react-router-dom";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+      <h1>Welcome to the Bubble App </h1>
+      <Form>
+        <Field type="text" name="username" />
+        <Field type="password" name="password" />
+        <button type="submit">Login</button>
+      </Form>
+    </div>
   );
 };
 
-export default Login;
+const FormikForm = withRouter(
+  withFormik({
+    mapPropsToValues({ username, password }) {
+      return {
+        username: username || "",
+        password: password || ""
+      };
+    },
+    handleSubmit(values, { props, setStatus }) {
+      axiosWithAuth()
+        .post("login", values)
+        .then((res) => {
+          console.log(res);
+          // window.localStorage.setItem('token', res.data.payload);
+          // const login = res.data;
+          // setStatus(login);
+          // props.history.replace('/colors')
+        })
+        .catch((err) => console.log(err));
+    }
+  })(Login)
+);
+
+export default FormikForm;
